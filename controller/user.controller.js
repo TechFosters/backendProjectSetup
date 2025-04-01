cabb3b1739b738955c3bdc63766d5cdc8eaea90c
TechFosters <akkihere8@gmail.com>
2025-04-01 22:56:47 +0530

    login, profile logout, forgot password functionalities

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -3,6 +3,7 @@ import crypto from "crypto"
 import nodemailer from "nodemailer"
 import bcrypt from "bcryptjs"
 import jwt from "jsonwebtoken"
+import cookieParser from "cookie-parser"
 const registerUser = async(req, res) => {
     //res.send('Registered!');
 
@@ -127,12 +128,18 @@ user.isVerified = true
 
 user.verificationToken = undefined
 
+
+res.status(201).json({
+    message: "User verified successfully",
+    success: true
+  })
+
 //step7: save
 await user.save()
 
 }
 
-//login controller
+/*login controller
  const login = async(req,res) =>{
     //Step1: get data
     const {email, password} = req.body
@@ -140,7 +147,7 @@ await user.save()
     //Step2: validate
 
     if(!email || !password){
-        return res.status(400).json({
+         return res.status(400).json({
             message: 'invalid username or password'
         })
     }
@@ -148,8 +155,9 @@ await user.save()
     //Step3: check in db is user exists
     try {
         const user = await User.findOne({email})
+        // console.log(user)
         if(!user){
-            res.status(400).json({
+            return res.status(400).json({
             message: 'invalid username or password'
             })
         }
@@ -160,7 +168,7 @@ await user.save()
 
     if(!isMatch){
         
-            res.status(400).json({
+            return res.status(400).json({
             message: 'invalid username or password'
             })
         
@@ -168,14 +176,14 @@ await user.save()
 
     const token = jwt.sign({id: user._id, role: user.role}, 'shhhhh',{
         expiresIn: '24h'
-    })
+    });
     const cookieOptions = {
         httpOnly: true,
         secure: true,
         maxAge: 24*60*60*1000
     }
-    res.cookie("token", token, {cookieOptions})
-    
+    res.cookie("token", token, cookieOptions)
+    console.log(token)
     
     res.status(200).json({
         success: true,
@@ -185,12 +193,188 @@ await user.save()
             id: user._id,
             name: user.name,
             role: user.role
-        },
+        }
     });
 }
     catch (error) {
+        res.status(400).json({
+            success: false,
+            message: 'login UNSUCCESSFUL',
+            error
         
+        })
     }
     
  }
-export {registerUser, verifyUser, login};
+    */
+ const login = async (req, res) => {
+    const { email, password } = req.body;
+  
+    if (!email || !password) {
+      return res.status(400).json({
+        message: "All fields are required",
+      });
+    }
+  
+    try {
+      const user = await User.findOne({ email });
+      if (!user) {
+        return res.status(400).json({
+          message: "Invalid email or password",
+        });
+      }
+  
+      const isMatch = await bcrypt.compare(password, user.password);
+  
+      console.log(isMatch);
+  
+      if (!isMatch) {
+        return res.status(400).json({
+          message: "Invalid email or password",
+        });
+      }
+  
+      //
+  
+      const token = jwt.sign(
+        { id: user._id, role: user.role },
+  
+        process.env.JWT_SECRET,
+        {
+          expiresIn: "24h",
+        }
+      );
+      const cookieOptions = {
+        httpOnly: true,
+        secure: true,
+        maxAge: 24 * 60 * 60 * 1000,
+      };
+      res.cookie("token", token, cookieOptions);
+  
+      res.status(200).json({
+        success: true,
+        message: "Login successful",
+        token,
+        user: {
+          id: user._id,
+          name: user.name,
+          role: user.role,
+        },
+      });
+    } catch (error) {
+        console.error("Login Error:", error);
+        res.status(400).json({
+            success: false,
+            message: "Login Unsuccessful",
+            error: error.message,
+        });
+    }
+  };
+  //profile
+  const getMe = async(req, res) =>{
+
+    try {
+        //const date = req.user
+        //console.log('reached at profile level', data);  
+        const user = await User.findById(req.user.id).select('-password')
+        if(!user){
+            return res.status(400).json({
+                success: false,
+                message: 'User not found'
+            })
+        }
+        res.status(200).json({
+            success: true,
+            user
+        })
+    } catch (error) {
+        
+    }
+  }
+  //logout user
+  const logoutUser = async(req, res) =>{
+    try {
+        res.cookie('token', '');
+        res.status(200).json({
+            success: true,
+            message: 'logged out successfully'
+        })
+    } catch (error) {
+        
+    }
+  }
+
+
+  //reset password
+  const resetPassword = async(req, res) =>{
+    try {
+        
+    } catch (error) {
+        
+    }
+  }
+  //forgetpassword
+  const forgotPassword = async(req,res) =>{
+    //get email
+    const {email} = req.body;
+
+   
+    try {
+         //find user based on email
+    const user = await User.findOne({email})
+    if(!user){
+        return res.status(400).json({
+            success: false,
+            message: 'User not found'
+        })
+    }
+
+    //reset Token
+    const token = crypto.randomBytes(32).toString("hex")
+    console.log(token);
+    user.resetPasswordToken = token;
+    console.log(user.resetPasswordToken);
+        
+    //resetTokenEXpires
+    user.resetPasswordExpires = Date.now()+ 10 * 60 * 1000;
+
+
+         //step7: Send token to user in email
+         const transporter = nodemailer.createTransport({
+            host: process.env.MAILTRAP_HOST,
+            port: process.env.MAILTRAP_PORT,
+            secure: false, // true for port 465, false for other ports
+            auth: {
+              user: process.env.MAILTRAP_USERNAME,
+              pass: process.env.MAILTRAP_PASSWORD
+            },
+          });
+    
+          const mailOption = await transporter.sendMail({
+            from: process.env.MAILTRAP_SENDEREMAIL, // sender address
+            to: user.email, // list of receivers
+            subject: "Forgot PASSWORD!?", // Subject line
+            text: `Please click on the following link ${process.env.BASE_URL}/api/v1/users/forgotPassword/${token}`, // plain text body
+            html: "<b>Hello world?</b>", // html body
+          });   
+    
+          await transporter.sendMail(mailOption); 
+    
+          res.status(201).json({
+            message: "Email sent to reset password",
+            success: true
+          })
+    
+    
+    //save
+    user.save()
+    } catch (error) {
+        console.error("Error:", error);
+        res.status(400).json({
+            success: false,
+            message: "forgot password reset unsuccessful",
+            error: error.message
+    })
+  }
+}
+export {registerUser, verifyUser, login, getMe, logoutUser, forgotPassword, resetPassword};
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,13 @@
 import express from 'express';
-import { registerUser, verifyUser, login } from '../controller/user.controller.js';
+import { isLoggedIn } from '../middleware/auth.middleware.js';
+import { registerUser, verifyUser, login, getMe, logoutUser, forgotPassword } from '../controller/user.controller.js';
 
 const router = express.Router()
 
 router.post("/register", registerUser)
 router.get("/verify/:token", verifyUser) //GET TOKEN FROM USER const {token} = req.params
 router.post("/login", login)
+router.get("/me", isLoggedIn, getMe)//jaise hi /me route pe aaenge phle isLoggedIn check hoga then getMe
+router.post("/logout", isLoggedIn, logoutUser) 
+router.post("/forgotPassword", forgotPassword)
 export default router;
